11c9f38d1e07fa4db806b3cafcba6957bdd50e05
squishram <icostello@pm.me>
2024-02-23 10:55:14 +0000

    starting nixos

---
+++ .config/nixos/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "squish's first ever special snowflake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      };
+  };
+
+  # remember to add `home-manager` to this list if you start using it
+  outputs = { self, nixpkgs, ... }:
+
+  let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+
+    {
+
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      squish = home-manager.lib.homeManagerConfiguration {
+        inherit system;
+        modules = [ ./home.nix ];
+      };
+    };
+
+  };
+
+}
--- README.md
+++ README.md
@@ -20,3 +20,33 @@ git clone git@github.com/squishram/.dotfiles.git
 cd .dotfiles
 stow .
 ```
+
+## Migration to NixOS Setup
+
+I am currently in the process of migrating to a declarative configuration using **NixOS** with **flake** and **home-manager**. These are the things I need to to, and the software I still need to install & configure.
+
+## To Do
+
+- Configure a Flake
+- Configure home-manager
+
+## Software still needed
+
+- Zathura
+- Bibtex
+- Biber
+- Unzip
+- Go
+- composer
+- php
+- javac
+- java
+- julia
+- python
+- pip
+- venv
+- cargo
+- luarocks
+- node
+- xclip (for x11) or wl-copy & wl-paste (for wayland)
+- pynvim
